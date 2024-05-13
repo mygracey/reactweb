c002af375bd6aab75e181e6e6dbcc99296d5eccc
grace <graceogwe@gmail.com>
2024-05-13 12:06:53 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import "./App.css"
+export default function App(){
+  return(
+    <div>
+      <h3>My Register Form App</h3>
+
+      <form>
+        <input type="text" placeholder="Create username"/><br />
+        <input type="password" placeholder="Create password"/><br />
+
+        <button>Create Account</button>
+      </form>
+
+
+
+
+
+    </div>
+  )
+}
